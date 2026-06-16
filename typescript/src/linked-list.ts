741fe37c1424dba41a9de45d405655749d41ae0c
Oscar da Silva <oscarkaka222@gmail.com>
2026-06-15 21:13:12 -0300

    Update linked-list.ts

--- typescript/src/linked-list.ts
+++ typescript/src/linked-list.ts
@@ -42,6 +42,15 @@ class LinkedList<T> {
     current.next = current?.next?.next;
     this.count--;
   }
+
+  indexOf(data: T): number {
+    if (this.count === 0) return -1
+    if (this?.head?.data === data) return this?.head?.index
+    
+    let current = this.head as CNode<T>
+    while (current.next !== null && current.next.data !== data) current = current?.next!
+    return current?.next?.index!
+  }
 }
 
 const myLL = new LinkedList<string>()
